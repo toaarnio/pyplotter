0b8025ed13df6f532a5d626a6d1a3ca85e9b1090
Tomi Aarnio <tomi.p.aarnio@gmail.com>
2020-05-08 16:15:05 +0300

    [sample] Update time series up to 2020-05-07

--- sample.py
+++ sample.py
@@ -15,8 +15,8 @@ import pyplottr       # local import
 
 
 START_DATE = "2020-01-21"
-N = 14   # predict from this many days of data
-P = 120  # predict this many days from START_DATE
+N = 70   # predict from this many days of data
+P = 110  # predict this many days from START_DATE
 
 DEATHS = [
     6,
@@ -64,31 +64,69 @@ DEATHS = [
     3202,
     3285,
     3387,
-    3494,
-    3599,
-    3827,
-    4025,
-    4296,  # Mar 10
-    4628,
-    4981,
-    5428,
-    5833,
-    6520,
-    7162,
-    7979,  # Mar 17
-    8951,
-    10030,
-    11386,
-    13011,
-    14640,
-    16513,
-    18894,  # Mar 24
-    21282,
-    24073,
-    27343,
-    30861,
-    34065,
-    37788,  # Mar 30
+    3493,
+    3598,
+    3826,
+    4023,
+    4297,  # Mar 10
+    4627,
+    4980,
+    5427,
+    5841,
+    6532,
+    7180,
+    8000,  # Mar 17
+    8983,
+    10077,
+    11457,
+    13101,
+    14739,
+    16671,
+    19157,  # Mar 24
+    21746,
+    24691,
+    28160,
+    31831,
+    35714,
+    39334,  # Mar 30
+    44043,
+    49233,
+    55503,
+    61465,
+    67531,
+    72535,
+    78141,  # Apr 6
+    86033,
+    92768,
+    100434,
+    107781,
+    113989,
+    119554,
+    125193,  # Apr 13
+    132601,
+    140791,
+    147789,
+    156218,
+    162894,
+    167788,
+    173277,  # Apr 20
+    180561,
+    187259,
+    193964,
+    200381,
+    206482,
+    210239,
+    214748,  # Apr 27
+    221436,
+    228029,
+    233824,
+    239447,
+    244644,
+    248144,
+    252240,  # May 4
+    258026,
+    264837,
+    270426,  # May 7
 ]
 
 
@@ -167,21 +205,28 @@ CASES = [
 
 
 def quadratic(x, a, b, c):
-    return a * x**2 + b * x + c
+    return np.clip(a * x**2 + b * x + c, 0, 5e6)
+
+
+def cubic(x, a, b, c, d):
+    return np.clip(a * x**3 + b * x**2 + c * x + d, 0, 5e6)
 
 
 def main():
     fig = pyplottr.Figure(f"Cumulative cases and deaths", ncols=2)
     for i, (title, ydata) in enumerate(zip(["Cases", "Deaths"], [CASES, DEATHS])):
-        # extrapolate with a second-degree polynomial
+        # extrapolate with second-degree & third-degree polynomials
         xdata = np.arange(0, len(ydata))
         xdata_extrapolated = np.arange(0, P+1)
-        popt, _ = scipy.optimize.curve_fit(quadratic, xdata[:N], ydata[:N])
-        pstr = f"prediction: ${popt[0]:.0f}x^2 {popt[1]:+.0f}x {popt[2]:+.0f}$"
+        pquad, _ = scipy.optimize.curve_fit(quadratic, xdata[:N], ydata[:N])
+        pstr_quad = f"prediction: ${pquad[0]:.1f}x^2 {pquad[1]:+.1f}x {pquad[2]:+.0f}$"
+        pcubic, _ = scipy.optimize.curve_fit(cubic, xdata[:N], ydata[:N])
+        pstr_cubic = f"prediction: ${pcubic[0]:.2f}x^3 {pcubic[1]:+.1f}x^2 {pcubic[2]:+.0f}x {pcubic[3]:+.0f}$"
         # plot the observed data points + prediction curve
         fig.axes[i].scatter(xdata[:N], ydata[:N], color="blue", label=f"training data, N={N}")
-        fig.axes[i].scatter(xdata[N:], ydata[N:], color="red", label=f"ground truth, N={len(ydata) - N}")
-        fig.axes[i].plot(xdata_extrapolated, quadratic(xdata_extrapolated, *popt), color="green", alpha=0.5, label=pstr)
+        fig.axes[i].scatter(xdata[N:], ydata[N:], color="red", label=f"ground truth, N={len(ydata)}")
+        fig.axes[i].plot(xdata_extrapolated, quadratic(xdata_extrapolated, *pquad), color="green", alpha=0.5, label=pstr_quad)
+        fig.axes[i].plot(xdata_extrapolated, cubic(xdata_extrapolated, *pcubic), color="blue", alpha=0.5, label=pstr_cubic)
         # prettify the graph with ticks, grids, labels, etc.
         fig.axes[i].xaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator(4))
         fig.axes[i].yaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator(4))
@@ -189,6 +234,7 @@ def main():
         fig.axes[i].ticklabel_format(axis="y", style="scientific", scilimits=(3, 3))
         fig.axes[i].grid(which="both")
         fig.axes[i].set_title(f"{title} (thousands)")
+        fig.axes[i].set_ylim(0, np.ceil(np.max(ydata) / 1e5) * 1e5)
         fig.axes[i].legend()
     fig.resize(1024, 768)
     fig.show()
